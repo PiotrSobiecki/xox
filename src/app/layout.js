a081fb8f1357c43e64b75108057608f99ae3438a
PiotrSobiecki <piotr.sobiecki@gameswift.io>
2024-11-24 14:38:43 +0100

    add pl

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,10 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>{children}</body>
     </html>
   );
